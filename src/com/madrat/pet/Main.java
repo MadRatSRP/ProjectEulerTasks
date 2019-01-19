4497f0f2cf49ba23870911c839bb769e1573d081
MadRat <mischa.alpeew@yandex.ru>
2019-01-19 22:00:22 +0300

    Hm...

--- src/com/madrat/pet/Main.java
+++ src/com/madrat/pet/Main.java
@@ -2,7 +2,38 @@ package com.madrat.pet;
 
 public class Main {
 
+    private static int sum = 0;
+
     public static void main(String[] args) {
-	// write your code here
+        printMultiplesOfThreeAndFiveFromTen();
+        printMultiplesOfThreeAndFiveFromOneHundred();
+        printMultiplesOfThreeAndFiveFromOneThousand();
+    }
+
+    public static void printMultiplesOfThreeAndFiveFromTen() {
+        for (int i = 1; i < 10; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.print(sum + "\n");
+    }
+
+    public static void printMultiplesOfThreeAndFiveFromOneHundred() {
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.print(sum + "\n");
+    }
+
+    public static void printMultiplesOfThreeAndFiveFromOneThousand() {
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.print(sum + "\n");
     }
 }
